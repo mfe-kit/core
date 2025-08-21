6ae28b1fbaac51ae35e0a7acf17cba7018a5e1d0
Serhii Kuzmin <erngorn@gmail.com>
2025-08-21 21:48:49 +0300

    add shared configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,4 @@
+import eslintConfig from '@mfe-kit/dev-tools/eslint.config.js';
 import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
 
-export default defineConfig([
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    rules: {
-      semi: 'error',
-      'prefer-const': 'error',
-      '@typescript-eslint/no-unsafe-function-type': 'off',
-    },
-  },
-]);
+export default defineConfig([...eslintConfig]);
